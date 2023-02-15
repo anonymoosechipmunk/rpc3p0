6d7eb93107925c36a41caefa58e87ecaeae571bd
Anonymoose Chipmunk <62297434+anonymoosechipmunk@users.noreply.github.com>
2023-02-15 12:41:36 -0500

    Create tasks.json (#1)
    * Create tasks.json
    
    * Update tasks.json
    
    * Update import-all-endpoints.ts and react-ssr.tsx
    
    * Update 4 files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,39 +1,34 @@
-import React from 'react';
-import './App.css'
-import { Demo1ImportingFunction } from './Demo1ImportingFunction';
+import React from "react";
+import "./App.css";
 
-import { Demo1ImportingType } from './Demo1ImportingType';
-import { Demo2 } from './Demo2';
-import { Demo3 } from './Demo3';
+import { Demo1ImportingFunction } from "./Demo1ImportingFunction";
+import { Demo2 } from "./Demo2";
+import { Demo3 } from "./Demo3";
 
-const Page: React.FC<{}> = props => {
+const Page: React.FC<{}> = (props) => {
   return (
     <div className="App">
-      <header className="App-header">
-        { props.children }
-      </header>
+      <header className="App-header">{props.children}</header>
     </div>
-  )
-}
+  );
+};
 
 function App() {
-  return (<>
-    <Page>
-        <Demo1ImportingType />
-    </Page>
+  return (
+    <>
+      <Page>
+        <Demo1ImportingFunction />
+      </Page>
 
-    <Page>
-      <Demo1ImportingFunction />
-    </Page>
-
-    <Page>
+      <Page>
         <Demo2 />
-    </Page>
+      </Page>
 
-    <Page>
+      <Page>
         <Demo3 />
-    </Page>
-  </>);
+      </Page>
+    </>
+  );
 }
 
 export default App;
--- transparent-typed-rpc/index.js
+++ transparent-typed-rpc/index.js
@@ -1,39 +1,43 @@
-
 async function rpc(route, args) {
-    const result = await fetch(`http://localhost:9001/api/v1/${route}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        mode: 'cors',
-        credentials: 'include',
-        body: JSON.stringify(args)  
-    });
-    if (result.status === 200) {
-        return await result.json();
+  const result = await fetch(
+    `https://${window.location.host.replace(/-3000/, "-9001")}/api/v1/${route}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      mode: "cors",
+      credentials: "include",
+      body: JSON.stringify(args),
     }
+  );
+  if (result.status === 200) {
+    return await result.json();
+  }
 
-    let error_detail = null;
-    try {
-        error_detail = await result.json();
-    } catch (error) {}
+  let error_detail = null;
+  try {
+    error_detail = await result.json();
+  } catch (error) {}
 
-    console.error(`[RPC ${route}]`, result, error_detail);
-    throw new Error(`[RPC ${route}] ${error_detail?.message || ""}`);
+  console.error(`[RPC ${route}]`, result, error_detail);
+  throw new Error(`[RPC ${route}] ${error_detail?.message || ""}`);
 }
 
 function rpcFactory(route) {
-    return async (args) => {
-        return await rpc(route, args);
-    };
+  return async (args) => {
+    return await rpc(route, args);
+  };
 }
 
-const proxy = new Proxy({}, {
-    get: function(oTarget, key) {
-        // FIXME "key" could be a symbol, maybe, and we should handle that 
-        return rpcFactory(key);
-    }
-});
+const proxy = new Proxy(
+  {},
+  {
+    get: function (oTarget, key) {
+      // FIXME "key" could be a symbol, maybe, and we should handle that
+      return rpcFactory(key);
+    },
+  }
+);
 
 module.exports = proxy;
-
